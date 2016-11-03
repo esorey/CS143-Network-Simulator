dca060fa9d863f02b464bd3e0a4b1bb35fae4b12
amaltariq <SeemaMacbook@MacBook-Pro.local>
2016-11-03 14:57:11 -0700

    Added some comments and skeleton function definitions for event queue and event

--- event.py
+++ event.py
@@ -1,1 +1,9 @@
 class event:
+	def __init__(self, event_type, time):
+		'''
+			event_type - enumerated type that indicates what sort of event 
+						 this is 
+			time - integer, when the event occurs
+		'''
+		self.type = event_type
+		self.time = time
--- event_queue.py
+++ event_queue.py
@@ -1,1 +1,22 @@
 class event_queue:
+	def __init__(self, event):
+		'''
+			Need some way to hold events
+		'''
+
+	def dequeue(self):
+		'''
+			return most recently enqueued event (sort by time property of
+			events)
+		'''
+
+	def enqueue(self, event):
+		'''
+			enqueue the event
+			maybe sort here? or sort in dequeue? based on time property
+		'''
+
+	def isempty(self):
+		'''
+			return self.data == []
+		'''
