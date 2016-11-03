aa72fe7e5bf765e48e4e01d514d1f1f0c8e81f2a
Shen <siwang@caltech.edu>
2016-11-03 00:03:26 -0700

    Attributes and Functions
    ID, routing table, bellmanFord function

--- router.py
+++ router.py
@@ -1,1 +1,13 @@
+from link import linkqueue()
+import flow
 class router:
+	"""Router: end points of the network"""
+	def __init__(self, id):
+		super(Host, self).__init__()
+		self.id = id
+		self.routingTable = []
+
+	'''Bellman-Ford Algorithm: Update routing tables based
+	on congestion information'''
+	def bellmanFord():
+		# run bellman ford
