48e3390a4c5b46be12289e9d55c5a08d93876a9b
amaltariq <amalftariq@gmail.com>
2016-11-08 09:39:36 -0800

    fixed small bugs in flows functions (some were pseudocode), there are still some issues with hostSendPckts (where do we get the packets to send). Added a constant.py file that holds constants used across classes

---
+++ constants.py
@@ -0,0 +1,6 @@
+# constants.py
+# Contains the constants that other classes and files will use
+
+MBTOBYTES = 1000000			# Convert megabytes to bytes
+DATA_PKT_SIZE = 1024		# Bytes per data packet
+ACK_PKT_SIZE = 64			# Bytes per acknowledgement packet
--- flow.py
+++ flow.py
@@ -1,15 +1,21 @@
-from host import sendPackets 
+from host import sendPackets
+import constants
+
 class flow:
 	"""Flow Class"""
-	def __init__(self, source, destination, data, start):
+	def __init__(self, source, destination, data_amt, start, ):
 		super(ClassName, self).__init__()
 		self.source = source
 		self.dest = destination
-		self.data = data # data
-		self.start = start # time at which flow begins
-		self.windowSize = 0 # set in congestion control algorithm 
-		self.currACK = 0 # the last acknowledged packet ID
-		self.droppedPackets = [] # dropped packets (IDs)
+		self.data_amt = data_amt	# Size of data in MB
+		self.start = start 			# Time at which flow begins
+		
+		self.windowSize = 0 		# set in congestion control algorithm 
+		self.currACK = 0 			# the last acknowledged packet ID
+		self.droppedPackets = [] 	# dropped packets (IDs)
+
+		# Number of data packets the flow needs to send
+		self.num_packets = data_amt * constants.MBTOBYTES / constants.DATA_PKT_SIZE
 		
 	def congestionControlAlg(pcktReceived, pcktSent): 
 		# run congestion control alg
@@ -18,17 +24,21 @@ class flow:
 		windowSize = 100
 		return windowSize
 
-	def hostSendPckts(): 
-		sendPckts = []
-		if len(droppedPackets) != 0: 
+	def hostSendPckts(self): 
+		packets_to_send = []
+		if len(self.droppedPackets) != 0: 
 			# send packets from dropped packets
-			if len(droppedPackets) >= windowSize:
-				sendPckts = droppedPackets[:windowSize]
-			elif len(droppedPackets) < windowSize:
-				sendPckts.append(droppedPackets)
+			if len(self.droppedPackets) >= self.windowSize:
+				packets_to_send = self.droppedPackets[:(self.windowSize)]
+			elif len(self.droppedPackets) < self.windowSize:
+				sendPckts.append(self.droppedPackets)
 				temp = windowSize - len(droppedPackets)
+
+				# This needs to be fixed (where to get the other packets to
+				#	send?)
 				sendPckts.append(other[temp:])
 			else:
+				# This also needs to be fixed
 				sendPckts.append(other[:windowSize])
 
 	def sendPckts(packets, destination): 
@@ -37,14 +47,14 @@ class flow:
 	''' When a host receives an acknowledgement packet it will call this 
 	function for the flow to update what packets have been received. The 
 	flow deals with packet loss.'''
-	def getACK(packetID):
-		if packetID  > currACK+1:  // dropped a packet
-			append(droppedPacket, range(currACK+1, packetID))
-		currACK += 1
-		elif packetID < currACK: 
-		Delete packetID from droppedPacket
+	def getACK(self, packetID):
+		if packetID  > currACK+1:  # if we dropped a packet
+			# Add the packets we dropped to the droppedPackets list
+			self.droppedPackets.append(range(currACK+1, packetID))
+			currACK += 1
+		elif packetID < currACK: 	# If we receive an ack for packet that was
+									# 	dropped
+			# Remove this packet from list of dropped packets
+			self.droppedPackets.remove(packetID)
 		else:
-		currACK += 1
-
-# Global ID variable for packet ID
-# Check by flow
+			currACK += 1 	# We received correct packet, increment currACK
