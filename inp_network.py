e0fc022ab8804882ec2c6aa7494d911d732e71d7
schen5050 <schen5050@gmail.com>
2016-11-10 00:02:56 -0800

    updated to include host parameters

--- inp_network.py
+++ inp_network.py
@@ -1,12 +1,10 @@
 from link import Link
 from flow import flow
+from host import host
 
-def inp_network(file, L=[], F=[]):
+def inp_network(file, L=[], F=[], H=[], R=[]):
     # Open relevant file
     f = open(file, 'r')
-    # Dictionary of links and flows to hold data while reading from file
-    links = {}
-    flows = {}
     # Initialize section count (each section has a specific format)
     sec_count = 0
     # Read each line in the file
@@ -21,38 +19,37 @@ def inp_network(file, L=[], F=[]):
         # Remove extra spaces
         while '' in params:
             params.remove('')
-        # Set dictionary values based on the section we're in
-        # What link is connecting
+        # Set list values based on the section we're in
+        # Update link and host/router parameters
+        # Return these instances in arrays: flows, links, hosts, routers
         if sec_count == 0:
-            links[params[1]] = [params[0],params[2][0:2]]
-        # What the link parameters are
-        if sec_count == 1:
-            links[params[0]].append(float(params[1]))
-            links[params[0]].append(float(params[2]))
-            links[params[0]].append(float(params[3]))
+            temp_link = Link(params[0]+'a',float(params[3]),float(params[4]),params[1],params[2],float(params[5]))
+            L.append(temp_link)
+            temp_link = Link(params[0]+'b',float(params[3]),float(params[4]),params[2]),params[1],float(params[5]))
+            L.append(temp_link)
+            if params[1][0] == 'H':
+                H.append(host(params[1],params[0]+'a'))
+            elif params[1][0] == 'R':
+                pass
+            if params[2][0] == 'H':
+                H.append(host(params[2],params[0]+'b'))
+            elif params[2][0] == 'R':
+                pass
+
         # Flow parameters
-        if sec_count == 2:
-            flows[params[0]] = [params[1], params[2], float(params[3]), float(params[4])]
+        if sec_count == 1:
+            F.append(flow(params[0],params[1],params[2],params[3],params[4]))
     f.close()
-    # Set class instances to the parameters from file
-    # Return these instances in arrays, one for flow one for link
-    for link in links:
-        print link
-        temp_link = Link(link+'a',links[link][2],links[link][3],links[link][0],links[link][1],links[link][4])
-        L.append(temp_link)
-        temp_link = Link(link+'b',links[link][2],links[link][3],links[link][0],links[link][1],links[link][4])
-        L.append(temp_link)
-    for fl in flows:
-        temp_flow = flow(fl,flows[fl][0],flows[fl][1],flows[fl][2],flows[fl][3])
-        F.append(temp_flow)
 
 '''
 # Test code
 fil = 'C:\Users\Sophia\Documents\GitHub\CS143-Network-Simulator\inp2.txt'
-lnks = []
-flws = []
+links = []
+flows = []
+hosts = []
+routers = []
 l = {}
-inp_network(fil,lnks,flws,l)
+inp_network(fil,links,flows,hosts,routers)
 print lnks
 print flws
 '''
