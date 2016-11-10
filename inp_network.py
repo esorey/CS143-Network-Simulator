d6c0b4e8f58bd0ba65bdff6d3de598cf9d1b5f08
schen5050 <schen5050@gmail.com>
2016-11-09 22:31:07 -0800

    updated network input to incorporate classes

--- inp_network.py
+++ inp_network.py
@@ -1,5 +1,10 @@
-def inp_network(file, links={}, flows={}):
+from link import Link
+from flow import flow
+
+def inp_network(file, L=[], F=[], links={}):
     f = open(file, 'r')
+    #links = {}
+    flows = {}
     sec_count = 0
     for line in f:
         # keep track of which section we're in
@@ -17,13 +22,24 @@ def inp_network(file, links={}, flows={}):
             links[params[0]].append(float(params[3]))
         if sec_count == 2:
             flows[params[0]] = [params[1], params[2], float(params[3]), float(params[4])]
+    f.close()
+    for link in links:
+        print link
+        temp_link = Link(link+'a',links[link][2],links[link][3],links[link][0],links[link][1],links[link][4])
+        L.append(temp_link)
+        temp_link = Link(link+'b',links[link][2],links[link][3],links[link][0],links[link][1],links[link][4])
+        L.append(temp_link)
+    for fl in flows:
+        temp_flow = flow(fl,flows[fl][0],flows[fl][1],flows[fl][2],flows[fl][3])
+        F.append(temp_flow)
 
 '''
 # Test code
 fil = 'C:\Users\Sophia\Documents\GitHub\CS143-Network-Simulator\inp2.txt'
-lnks = {}
-flws = {}
-inp_network(fil,lnks,flws)
+lnks = []
+flws = []
+l = {}
+inp_network(fil,lnks,flws,l)
 print lnks
 print flws
 '''
