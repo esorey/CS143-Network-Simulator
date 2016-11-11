c8e83e7b46fe87c5eb3e76c8032fa6aef6272600
amaltariq <amalftariq@gmail.com>
2016-11-11 01:54:06 -0800

    comment about routing table packets from meeting

--- packet.py
+++ packet.py
@@ -11,7 +11,7 @@ class RoutingTablePacket(Packet):
     def __init__(self, packet_id, origin_id, size):
         # No destination because it needs to go to all neighbors of the origin node
         super().__init__(packet_id, origin_id, None, size)
-
+        # TODO: this packet should contain routing table information
 
 class DataPacket(Packet):
     def __init__(self, packet_id, origin_id, destination_id, pkt_flow):
