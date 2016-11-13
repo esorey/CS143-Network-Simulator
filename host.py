40e4c052c20fa1e443b4b4cb50ee4350d5ad0f6e
amaltariq <amalftariq@gmail.com>
2016-11-13 03:56:56 -0800

    modified host.py to fix variable names and turn system_EQ into constants.system_EQ

--- host.py
+++ host.py
@@ -13,23 +13,23 @@ class Host:
 	'''Add the passed packets to the link queue of the 
 	link it is connected to'''
 	def sendPackets(packetlist):
-		for pkt in packetlist:
-			sendPckt = Event(Event.pckt_send, system_EQ.currentTime, [self.link, pkt])
-			system_EQ.enqueue(sendPckt)
+		for pckt in packetlist:
+			sendPckt = Event(Event.pckt_send, system_EQ.currentTime, [self.link, pckt])
+			constants.system_EQ.enqueue(sendPckt)
 
 	'''Receive the packets from the link queue'''
-	def receivePacket(packet):
-		if type(packet) is AckPacket: # Acknowledgment packet
+	def receivePacket(pckt):
+		if type(pckt) is AckPacket: # Acknowledgment packet
 			# make and enqueue an event for the event queue 
 			# for acknowledging a received acknowledgment packet
-			ackEvent = Event(Event.ack_rcv, system_EQ.currentTime, 
-				[packet.packet_id, packet.owner_flow])
-			system_EQ.enqueue(ackEvent)
+			ackEvent = Event(Event.ack_rcv, constants.system_EQ.currentTime, 
+				[pckt.packet_id, pckt.owner_flow])
+			constants.system_EQ.enqueue(ackEvent)
 
-		if type(packet) is DataPacket: # Data packet
+		if type(pckt) is DataPacket: # Data packet
 			# create an acknowledgment packet
-			ackpckt = AckPacket(packet.packet_id, packet.origin_id, packet.destination_id, flow.ID)
+			ackpckt = AckPacket(pckt.packet_id, pckt.origin_id, pckt.destination_id, flow.ID)
 			# push the new acknowledgment
-			sendAckPckt = Event(Event.pckt_send, system_EQ.currentTime, [self.link, ackpckt])
-			system_EQ.enqueue(sendAckPckt)
+			sendAckPckt = Event(Event.pckt_send, constants.system_EQ.currentTime, [self.link, ackpckt])
+			constants.system_EQ.enqueue(sendAckPckt)
 
