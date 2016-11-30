8c62df37c4e63f3e9d30e3bdebac574c6543e58c
amaltariq <amalftariq@gmail.com>
2016-11-29 22:26:15 -0800

    removed update_RENO event and added a packet timeout event

--- event.py
+++ event.py
@@ -1,26 +1,26 @@
 class Event:
-	flow_start = 1
-	pckt_rcv = 2
-	link_free = 3
-	flow_src_send_packets = 4
-	ack_rcv = 5
-	pckt_send = 6
-	update_FAST = 7
-	update_RENO = 8
+    flow_start = 1
+    pckt_rcv = 2
+    link_free = 3
+    flow_src_send_packets = 4
+    ack_rcv = 5
+    pckt_send = 6
+    update_FAST = 7
+    pckt_timeout = 8
 
-	def __init__(self, ev_type, time, data):
-		'''
-			event_type - enumerated type that indicates what sort of event 
-						 this is 
-			time - integer, when the event occurs
-			data - information required for the events (list of varying size
-				   depending on event, see below for details.
-		'''
-		self.event_type = ev_type
-		self.time = time
-		self.data = data
-		
-		''' Dealing with events:
+    def __init__(self, ev_type, time, data):
+        '''
+            event_type - enumerated type that indicates what sort of event 
+                         this is 
+            time - integer, when the event occurs
+            data - information required for the events (list of varying size
+                   depending on event, see below for details.
+        '''
+        self.event_type = ev_type
+        self.time = time
+        self.data = data
+        
+        ''' Dealing with events:
         pckt_rcv:
             needs: host, packet, time
                 changed to calling receive packet
@@ -43,5 +43,5 @@ class Event:
                 call flow.getACK
 
         pckt_send:
-        	needs: link, packet
-        		call link.enqueue_packet(packet)'''
+            needs: link, packet
+                call link.enqueue_packet(packet)'''
